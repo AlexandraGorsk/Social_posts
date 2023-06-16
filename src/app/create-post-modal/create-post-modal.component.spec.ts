31ebb79a5483fa71bd33bfd9901820ef2b3f8188
Alexandra <alex_gorskaya1@mail.ru>
2023-06-16 09:16:46 +0200

    add modules in test files

--- src/app/create-post-modal/create-post-modal.component.spec.ts
+++ src/app/create-post-modal/create-post-modal.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CreatePostModalComponent } from './create-post-modal.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('CreatePostModalComponent', () => {
   let component: CreatePostModalComponent;
@@ -8,9 +10,13 @@ describe('CreatePostModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreatePostModalComponent ]
-    })
-    .compileComponents();
+      declarations: [CreatePostModalComponent],
+      imports: [FontAwesomeModule, ReactiveFormsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreatePostModalComponent);
     component = fixture.componentInstance;
